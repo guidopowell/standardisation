c4e7fd4b89ba82853f460a33e12a668fd330bba2
guidopowell <33639746+guidopowell@users.noreply.github.com>
2024-10-28 16:25:37 -0400

    added GMF data

---
+++ R/donnees_GMF.R
@@ -0,0 +1,17 @@
+#' Données GMF santé mentale
+#'
+#' Données de patients inscrits dans un GMF ayant une condition de santé mentale 
+#'
+#' @docType data
+#'
+#' @usage data(donnees_GMF)
+#'
+#' @format Un object de classe \code{"tibble"};
+#'
+#' @keywords datasets
+#'
+#'
+#' @examples
+#' data(donnees_GMF)
+#' hist(donnees_sim$prop)
+"donnees_GMF"
